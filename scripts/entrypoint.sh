65123fbed6a52dfb0c72ef1a6f781a6356a436a3
Azkali <a.ffcc7@gmail.com>
2020-10-08 18:10:32 +0200

    Update: add tabulation to log, remove u-boot docker image as it's useless now.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -18,19 +18,19 @@ if [[ $(command -v 7z) ]]; then zip_ver=7z;
 	else [[ -z "${zip_ver}" ]] && \
 	echo "7z is not installed on your machine..." && exit 1; fi
 
-echo -e "\nBuilding boot.scr and initramfs and updating coreboot.rom\n"
+echo -e "\n\t\tBuilding boot.scr and initramfs and updating coreboot.rom\n"
 docker run -it --rm -e CPUS="${CPUS}" -e DISTRO="${DISTRO}" -e PARTNUM="${PARTNUM}" -e HEKATE_ID="${HEKATE_ID}" -v "${volume}":/out alizkan/l4t-bootfiles-misc:latest
 
-echo -e "\nBuilding L4T-Kernel\n"
+echo -e "\n\t\tBuilding L4T-Kernel\n"
 docker run -it --rm -e CPUS="${CPUS}" -v "${volume}":/out alizkan/l4t-kernel:latest
 
 # Copying kernel, kernel modules and device tree file to switchroot directrory
 mv "${out}/Image" "${out}/tegra210-icosa.dtb" "${out}/modules.tar.gz" "${out}/update.tar.gz" "${out}/switchroot/${DISTRO}"
 
-echo -e "\nBuilding the actual distribution\n"
+echo -e "\n\t\tBuilding the actual distribution\n"
 docker run -it --rm --privileged -e DISTRO="${DISTRO}" -e DEVICE="${DEVICE}" -e HEKATE=true -e HEKATE_ID="${HEKATE_ID}" -v "${volume}":/out alizkan/jet-factory:latest
 
-echo -e "\nUpdating and renaming 7z archive created during JetFactory build\n"
+echo -e "\n\t\tUpdating and renaming 7z archive created during JetFactory build\n"
 "${zip_ver}" u "${out}/switchroot-${DISTRO}.7z" "${out}/bootloader" "${out}/switchroot"
 
 # Add date for release tag.
@@ -38,4 +38,4 @@ mv "${out}/switchroot-${DISTRO}.7z" "${out}/switchroot-${DISTRO}-$(date +%F).7z"
 
 # Cleaning build files
 rm -r "${out}/bootloader/" "${out}/switchroot/"
-echo -e "Done, file produced: switchroot-${DISTRO}-$(date +%F).7z"
+echo -e "\n\t\tDone, file produced: switchroot-${DISTRO}-$(date +%F).7z\n"
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker pull alizkan/l4t-kernel:latest
-docker pull alizkan/switch-uboot:linux-norebase
 docker pull alizkan/l4t-bootfiles-misc:latest
 docker pull alizkan/jet-factory:latest
