359939bc0e51c91b4de419ae42e35ebfdf211dce
Azkali <a.ffcc7@gmail.com>
2020-10-24 23:53:39 +0200

    Update: remove HEKATE_ID

--- README.md
+++ README.md
@@ -16,14 +16,13 @@ Variables:
     CPUS=4	      Number of CPU threads to allcoate during build.
     DEVICE=tegra210   Device as set in the configs directory.
     DISTRO=arch       Target distribution using file found in DEVICE folder.
-    HEKATE_ID=SWR-ARC Set a Hekate ID.
     PARTNUM=mmcblk0p2 Set the partition that will be booted (it should reflect what you can find in`/dev`)
 ```
 
 ## The classic Docker way
 
 ```sh
-sudo DISTRO=arch DEVICE=tegra210 PARTNUM=mmcblk0p2 HEKATE_ID=SWR-ARC CPUS=4 ./scripts/entrypoint.sh /absolute/path/to/build/dir
+sudo DISTRO=arch DEVICE=tegra210 PARTNUM=mmcblk0p2 CPUS=4 ./scripts/entrypoint.sh /absolute/path/to/build/dir
 ```
 
 ## The Docker In Docker way
@@ -50,7 +49,7 @@ docker run --privileged -it --rm -v /var/run/docker.sock:/var/run/docker.sock al
 
 Run the container:
 ```sh
-docker run --privileged -it --rm -e DISTRO=arch -e DEVICE=tegra210 -e PARTNUM=mmcblk0p2 -e HEKATE_ID=SWR-ARC -e CPUS=4 -v "${swr_vol}":/out -v /var/run/docker.sock:/var/run/docker.sock alizkan/l4t-aio-distro-builder:latest
+docker run --privileged -it --rm -e DISTRO=arch -e DEVICE=tegra210 -e PARTNUM=mmcblk0p2 -e CPUS=4 -v "${swr_vol}":/out -v /var/run/docker.sock:/var/run/docker.sock alizkan/l4t-aio-distro-builder:latest
 ```
 
 Remove the volume when done:
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,8 +6,8 @@ if  [[ -f /.dockerenv ]]; then out=/out; volume=$(docker inspect -f '{{range .Mo
 	else out="$(realpath "${@:$#}")"; volume="${out}"; [[ ! -d ${out} ]] && echo "Invalid output path: ${out}" && exit 1; fi
 
 # Check if the environement variables are set
-[[ -z "${DISTRO}" || -z "${PARTNUM}" || -z "${HEKATE_ID}" || -z "${DEVICE}" ]] && \
-	echo "Invalid options: DISTRO=${DISTRO} PARTNUM=${PARTNUM} HEKATE_ID=${HEKATE_ID} DEVICE=${DEVICE}" && exit 1
+[[ -z "${DISTRO}" || -z "${PARTNUM}" || -z "${DEVICE}" ]] && \
+	echo "Invalid options: DISTRO=${DISTRO} PARTNUM=${PARTNUM} DEVICE=${DEVICE}" && exit 1
 
 # Check if docker is installed
 [[ ! $(command -v docker) ]] && \
@@ -20,7 +20,7 @@ if [[ $(command -v 7z) ]]; then zip_ver=7z;
 	echo "7z is not installed on your machine..." && exit 1; fi
 
 echo -e "\n\t\tBuilding boot.scr and initramfs and updating coreboot.rom\n"
-docker run -it --rm -e CPUS="${CPUS}" -e DISTRO="${DISTRO}" -e PARTNUM="${PARTNUM}" -e HEKATE_ID="${HEKATE_ID}" -v "${volume}":/out alizkan/l4t-bootfiles-misc:latest
+docker run -it --rm -e CPUS="${CPUS}" -e DISTRO="${DISTRO}" -e PARTNUM="${PARTNUM}" -v "${volume}":/out alizkan/l4t-bootfiles-misc:latest
 
 echo -e "\n\t\tBuilding L4T-Kernel\n"
 docker run -it --rm -e CPUS="${CPUS}" -v "${volume}":/out alizkan/l4t-kernel:latest
@@ -29,7 +29,7 @@ docker run -it --rm -e CPUS="${CPUS}" -v "${volume}":/out alizkan/l4t-kernel:lat
 mv "${out}/Image" "${out}/tegra210-icosa.dtb" "${out}/modules.tar.gz" "${out}/update.tar.gz" "${out}/switchroot/${DISTRO}"
 
 echo -e "\n\t\tBuilding the actual distribution\n"
-docker run -it --rm --privileged -e DISTRO="${DISTRO}" -e DEVICE="${DEVICE}" -e HEKATE=true -e HEKATE_ID="${HEKATE_ID}" -v "${volume}":/out alizkan/jet-factory:latest
+docker run -it --rm --privileged -e DISTRO="${DISTRO}" -e DEVICE="${DEVICE}" -e HEKATE=true -v "${volume}":/out alizkan/jet-factory:latest
 
 echo -e "\n\t\tUpdating and renaming 7z archive created during JetFactory build\n"
 "${zip_ver}" u "${out}/switchroot-${DISTRO}.7z" "${out}/bootloader" "${out}/switchroot"
